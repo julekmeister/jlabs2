aa6f6cc1145b42acb6e56b1101817e5f4a51d923
Julekmeister <julekmeister@gmail.com>
2021-11-07 16:26:22 +0100

    cw1

--- src/Main.java
+++ src/Main.java
@@ -10,28 +10,79 @@ public class Main {
         */
         int index = 59936; int i=1000;
         int n = (index%100);
+        int a;
         System.out.println(n);
-        while (i>0)
-        {
-            i=i-1;
-            if (i%n==0)
+        do {
+            a = (i % n);
+            switch (a)
             {
-                System.out.println("liczba " + i + " podzielna przez "  +n);
+                case 0:
+                    System.out.println("liczba " + i + " podzielna przez " + n);
+                    break;
+                /*default:
+                    System.out.println("liczba " + i + " niepodzielna przez " + n);*/
             }
-        }
+        }   while (--i>0);
+
+        System.out.println("\n");
 
         /* zad 2
         a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
           przeiterować tablicę po elementach i wyswietlić, użyć pętli while
+
         b) jak wyżej tylko dla tablicy typu double[],
         c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
         */
 
+        //a
+        int tab[]={10,200,3000,40000,5000};
+        i=0;
+        while ( i<5)
+        {
+            System.out.println(tab[i]);
+            i=i+1;
+        }
+        System.out.println("\n");
+
+        //b
+        double tab2[]={10.1,20.20,30.400,400.500,500.70};
+        i=0;
+        while ( i<5)
+        {
+            System.out.println(tab2[i]);
+            i=i+1;
+        }
+        System.out.println("\n");
+
+        //c
+        String tab3[]={"jeden","dwa","trzy","cztery","piec"};
+        i=4;
+        while ( i>=0)
+        {
+            System.out.println(tab3[i]);
+            i=i-1;
+        }
+        System.out.println("\n");
         /* zad.3
         a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true --> while(true)
         b) a jak gdy damy false --> while(false)
         */
 
+        //a
+        /*i=0;
+        while (true)
+        {
+            System.out.println(i);
+            ++i;
+        }*/
+
+        //b
+       /* i=0;
+        while (false)
+        {
+            System.out.println(i);
+            ++i;
+        }*/
 
     }
 }
