d0efad026e05703a8469982394a842cffac9686d
Julekmeister <julekmeister@gmail.com>
2021-11-07 15:50:41 +0100

    cw1

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,17 @@ public class Main {
           Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indeksu (podać nr indesu z uczelni)
           to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
         */
-
+        int index = 59936; int i=1000;
+        int n = (index%100);
+        System.out.println(n);
+        while (i>0)
+        {
+            i=i-1;
+            if (i%n==0)
+            {
+                System.out.println("liczba " + i + " podzielna przez "  +n);
+            }
+        }
 
         /* zad 2
         a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
