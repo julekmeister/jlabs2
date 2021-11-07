1512fa0256d9c13642a443f00babf4e8406fa92c
Julekmeister <julekmeister@gmail.com>
2021-11-07 16:51:07 +0100

    cw2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,11 +10,35 @@ public class Main {
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
+        int index = 59936;
+        int n = index%10;
+        int i;
+        System.out.println(n);
+
+        for ( i = 100;i>0;--i)
+        {
+            if (i%n==0)
+            {
+                System.out.println("Liczba "+i+" jest podzielna przez "+n);
+            }
+        }
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
 
+        int[] tab = new int[5];
+        Scanner x = new Scanner(System.in);
+        for (i=0;i<5;++i)
+        {
+            System.out.println("\nPodaj "+(i+1)+" element:");
+            tab[i]=  Integer.parseInt(x.nextLine());
+
+        }
+        for (int t:tab)
+        {
+            System.out.println(t+11);
+        }
 
     }
 }
