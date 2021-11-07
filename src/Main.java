4e9d284c141a3f36bb253739de743e6c5188e124
Julekmeister <julekmeister@gmail.com>
2021-11-07 17:19:18 +0100

    cw3

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,39 @@ public class Main {
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
 
+        //a
+        String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        int x = text.length();
+        int[] tab = new int[5];
+        int i=0;
+        for (char w : text.toCharArray()) {
+            switch (w) {
+                case 'o':
+                    ++tab[0];
+                    ++i;
+                    break;
+                case 'p':
+                    ++tab[1];
+                    ++i;
+                    break;
+                case 'x':
+                    ++tab[2];
+                    ++i;
+                    break;
+                case 'z':
+                    ++tab[3];
+                    ++i;
+                    break;
+                case 'q':
+                    ++tab[4];
+                    ++i;
+                    break;
+            }
+        }
+
+        System.out.printf("\nIlosc o: %d\nIlosc p: %d\nIlosc x: %d\nIlosc z: %d\nIlosc q: %d\n",
+                tab[0], tab[1], tab[2],tab[3],tab[4]);
+        System.out.println("Ilosc liter innych: "+(x-i));
 
     }
 }
