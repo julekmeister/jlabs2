76563eba964a888d93b6d28f3ffb2bad7a7909fe
Julekmeister <julekmeister@gmail.com>
2021-11-11 19:16:59 +0100

    cw5

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,76 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
+
+    //1-b
     public static int[] metoda1(int x){
-        int tab[]=new int[x];
+        int[] tab =new int[x];
         int j=40;
         for (int i=0;i<x;++i)
         {
             tab[i]=j-i;
-
         }
         return tab;
     }
-    public static int metoda2(int[] tab){
 
+    //1-c
+    public static void metoda2(int[] item){
 
-            int [] tab1 = new int [1];
+        for (int i:item
+             ) {
+            System.out.println(i);
+        }
 
-    } return tab1;
+    }
+    //2-b
+    public static String[] metoda3(int x){
+        String[] tab =new String[x];
+        Scanner s = new Scanner(System.in);
+
+        for (int i=0;i<x;++i)
+        {
+            System.out.println("\nPodaj element "+(i+1)+":");
+            tab[i] = s.nextLine();
+        }
+        return tab;
+    }
+    //2-c
+    public static void metoda4(String[] tab){
+        System.out.println("\nTablica:");
+        for (int i=5;i>=0;--i)
+        {
+            System.out.println(tab[i]);
+        }
+
+    }
+
+    //3-a
+        public static void metoda5(String Imie){
+        System.out.println(Imie);
+    }
+        //3-b
+        public static void metoda5(String Imie, String Nazwisko) {
+            System.out.println(Imie + " " + Nazwisko);
+        }
+        //3-c
+        public static void metoda5(String Imie, String Nazwisko, int wiek){
+            System.out.println(Imie + " " + Nazwisko + ", " + wiek);
+            }
+
+        //4-a
+        public static int metoda6(int x, int y){
+
+            return x + y;
+    }
+        //4-b
+        public static int metoda6(int x, int y, int z){
+
+            return x + y + z;
+        }
+
 
     public static void main(String[] args) {
         /* ćwiczenie 5 Metody */
@@ -31,7 +83,11 @@ public class Main {
         */
 
         //a
+        int[] tab =new int[20];
 
+        //d
+        tab=metoda1(20);
+        metoda2(tab);
 
 
 
@@ -44,6 +100,11 @@ public class Main {
         c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
         */
 
+        //a
+        String[] tab2 = new String[6];
+        tab2=metoda3(6);
+        metoda4(tab2);
+
         /* zad.3
         a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
             wyświetla w konsoli string który ma być waszym imieniem,
@@ -53,6 +114,9 @@ public class Main {
             nazwisko i wiek (int), a następnie wszystko wyświetli,
         */
 
+        //c
+        metoda5("Julian","Miśkiewicz",37);
+
         /* zad.4
         a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
             coś takiego:                                    zmiennaA = metoda(1, 2); <-- wywołanie metody
@@ -60,7 +124,8 @@ public class Main {
             którą przypiszemy do zmiennej, coś takiego:     zmiennaB = metoda(7, 8, 9); <-- wywołanie metody
         c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
         */
-
+        //c
+        System.out.println("wynik: " + (metoda6(8, 9) + metoda6(2, 3, 4)));
 
     }
 
