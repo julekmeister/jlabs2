083ceef14f6afb3f2b90a3ca5047ad0a44e7363b
Julekmeister <julekmeister@gmail.com>
2021-11-11 20:47:56 +0100

    cw5-1

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@ import java.util.*;
 
 
 public class Main {
-public static void Metoda1(String list){
-    for (String i:list<String>
+//1-b
+public static void Metoda1(List<String> lista){
+    System.out.println("\n");
+    for (String item:lista
          ) {
-
+        System.out.println(item);
     }
+    System.out.println("Liczba zwierząt: "+lista.size());
 }
     public static void main(String[] args) {
 
@@ -24,25 +27,35 @@ public static void Metoda1(String list){
                 d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
                 elementów listy
           */
-
+        //1-a
         List<String> lista = new ArrayList<>(5);
         Scanner scan = new Scanner(System.in);
-        int len;
 
         while (true){
         System.out.println("Podaj zwierzę: ");
         lista.add(scan.nextLine());
-        len=lista.size();
-        System.out.println("Liczba zwierząt: "+len);
-        System.out.println("Czy dodać kolejne zwierzę?\ty/n");
-        if (Objects.equals(scan.nextLine(), "n")){
-            if (len<5) {
-                System.out.println("Za mało zwierząt!");
-            }
-            else break;
+        System.out.println("Liczba zwierząt: "+lista.size());
+
+        if (lista.size()>=5){
+            System.out.println("Czy dodać kolejne zwierzę?\ty/n");
+            if (Objects.equals(scan.nextLine(), "n")){
+                break;
+                }
             }
         }
 
+        //1-c
+
+        lista.remove(lista.size()-1);
+        lista.remove(lista.size()-1);
+        lista.add("kapibara");
+        lista.add("salamandra plamista");
+        lista.add("zimorodek");
+        Metoda1(lista);
+
+        //1-d
+        lista.sort(Collections.reverseOrder());
+        Metoda1(lista);
 
          /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
                 niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
